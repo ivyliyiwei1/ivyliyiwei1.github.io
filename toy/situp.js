e6156b2d7202ceda8816b09616b09f314d4f08a7
Suny Gao <gaos@newschool.edu>
2020-11-06 10:47:16 -0500

    situp update

--- toy/situp.js
+++ toy/situp.js
@@ -1,16 +1,47 @@
+window.addEventListener("load", function() {
+    var element = document.getElementById('myimage');
+    var count = 0;
+    //count variable set to 0
 
+    document.addEventListener('keydown', onKeyDown);
+    document.addEventListener('keyup', onKeyUp);
 
-
-function changeimage()
-{
-    element=document.getElementById('myimage')
-    if (element.src.match("situp"))
-    {
-        element.src="sitdown.png";
+    function onKeyDown(evt) {
+        if(evt.keyCode === 32) {
+            element.src="situp.png";
+        }
     }
-    else
-    {
-        element.src="situp.png";
-};
-};
+
+    function onKeyUp(evt) {
+        if(evt.keyCode === 32) {
+            element.src="sitdown.png";
+            count();
+        }
+    }
+
+    function count() {
+        //add 1 to the count
+        count = count + 1;
+
+        if(count >= 50) {
+            //do something here
+            atFifty();
+        }
+
+        if(count >= 100) {
+
+        }
+    }
+
+    function atFifty() {
+
+    }
+
+
+    
+
+});
+
+
+
 
