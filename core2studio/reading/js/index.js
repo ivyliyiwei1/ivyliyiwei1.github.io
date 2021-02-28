8b2ac18bef31bb3ab6daf4277784f198cd15aea6
ivyliyiwei1 <71356272+ivyliyiwei1@users.noreply.github.com>
2021-02-28 14:44:00 +0800

    wohaolei

--- core2studio/reading/js/index.js
+++ core2studio/reading/js/index.js
@@ -1,3 +1,20 @@
-$(".shadows").hover(function () {
-    $(this).addClass("shadowsover");
-});
+var cubex = -22,    // initial rotation
+    cubey = -38,
+    cubez = 0;
+function rotate(variableName, degrees) {
+    window[variableName] = window[variableName] + degrees;
+    rotCube(cubex, cubey, cubez);
+}
+function rotCube(degx, degy, degz) {
+    segs = "rotateX(" + degx + "deg) rotateY(" + degy + "deg) rotateZ(" + degz + "deg) translateX(0) translateY(0) translateZ(0)";
+    $('#D3Cube').css({ "transform": segs });
+}
+function turnRight() {
+    rotate("cubey", 90);
+}
+function turnLeft() {
+    rotate("cubey", -90);
+}
+function flipCube() {
+    rotate("cubez", -180);
+}
