484f67fa6233d461852c74991d299a7a3bdf8282
ivyliyiwei1 <71356272+ivyliyiwei1@users.noreply.github.com>
2021-03-01 15:04:37 +0800

    wsl

--- core2studio/reading/js/index.js
+++ core2studio/reading/js/index.js
@@ -1,20 +1,14 @@
-var cubex = -22,    // initial rotation
-    cubey = -38,
-    cubez = 0;
-function rotate(variableName, degrees) {
-    window[variableName] = window[variableName] + degrees;
-    rotCube(cubex, cubey, cubez);
-}
-function rotCube(degx, degy, degz) {
-    segs = "rotateX(" + degx + "deg) rotateY(" + degy + "deg) rotateZ(" + degz + "deg) translateX(0) translateY(0) translateZ(0)";
-    $('#D3Cube').css({ "transform": segs });
-}
-function turnRight() {
-    rotate("cubey", 90);
-}
-function turnLeft() {
-    rotate("cubey", -90);
-}
-function flipCube() {
-    rotate("cubez", -180);
-}
+$(document).ready(function () {
+    $(window).scroll(function () {
+        $('.hideme').each(function (i) {
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            if (bottom_of_window > bottom_of_object) {
+                $(this).addClass('showme');
+            }
+            if (bottom_of_window < bottom_of_object) {
+                $(this).removeClass('showme');
+            }
+        });
+    });
+});
