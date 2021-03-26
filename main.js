98f622138d7b5fc5b8de31e0256abe009823276a
ivyliyiwei1 <71356272+ivyliyiwei1@users.noreply.github.com>
2021-03-26 14:56:59 +0800

    new

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 // general
 console.log("Ivy Li:)");
+// alertbox
+alert("Hello!This is Ivy's assignment page! OVO");
 // Show the list when I click the subject
 function showList1() {
+    // get the onclick element from HTML 
     var x = document.getElementById("wrapper1");
+    // if the display of wrapper is not in block 
     if (x.style.display != "block") {
+        // then change to block
         x.style.display = "block";
+        // if it is block, change to none(hidden)
     } else {
         x.style.display = "none";
     }
